f275debe3794af9c53c587ca60421c421d346e04
brandon-kingsinfo <brandon@kingsinfo.com.tw>
2022-11-17 16:38:14 +0800

    chapter 8 finished

--- name-mangling.py
+++ name-mangling.py
@@ -2,15 +2,23 @@ class Backpack:
     def __init__(self):
         self.__items = []
 
+    @property
+    def items(self):
+        return self.__items
+
     def add(self, item):
         self.__items.append(item)
 
     def pop(self, item):
-        for i in range(len(self.__items)):
-            if self.__items[i] == item:
-                return self.__items[i]
+        # for i in range(len(self.__items)):
+        #     if self.__items[i] == item:
+        #         return self.__items[i]
 
-        return None
+        if item in self.__items:
+            self.__items.remove(item)
+            return True
+
+        return False
 
 
 mybackpack = Backpack()
@@ -22,5 +30,7 @@ mybackpack.add("book")
 # non-public attributes are not meant to be accessde directly
 print(mybackpack._Backpack__items)
 
-print(mybackpack.pop("apple"))
-print(mybackpack.pop("laptop"))
+print("success") if mybackpack.pop("apple") else print("error findind apple in items")
+
+# sorted returns a new list without modifying the original one
+print(sorted(mybackpack.items))
